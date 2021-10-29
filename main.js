b15cfd19476e1e8d43e341f4160dfdfad8129f85
PerfectlyCube <93358074+PerfectlyCube@users.noreply.github.com>
2021-10-29 15:22:09 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,49 +1,104 @@
 const canvas = document.getElementById("screen");
 const g = canvas.getContext('2d');
 
-let speed = 10, drag = 100, velX = 0, velY = 0;
-let x = 0, y = 0;
+let speed = 6,
+  drag = 10,
+  velX = 0,
+  velY = 0;
+let x = 0,
+  y = 0;
+
+const Category = {
+
+  COMBAT: 0,
+  MOVEMENT: 1,
+  PLAYER: 2,
+  VISUAL: 3,
+
+}
 
 var controls = {
- 
+
   down: false,
   left: false,
   right: false,
   up: false
- 
+
 }
 
 function loop() {
- 
+
+  x += velX;
+  y += velY;
+
+  velX = clamp(velX, -speed, speed);
+  velY = clamp(velY, -speed, speed);
+
   g.fillStyle = "#ffffff";
   g.fillRect(0, 0, 800, 600);
-  
-  if(controls.down)
-    y += 4;
- 
+
+  if (controls.down)
+    velY += speed / drag;
+
+  if (controls.left)
+    velX -= speed / drag;
+
+  if (controls.right)
+    velX += speed / drag;
+
+  if (controls.up)
+    velY -= speed / drag;
+
+  if (!controls.left && !controls.right)
+    velX += velX / -drag;
+
+  if (!controls.up && !controls.down)
+    velY += velY / -drag;
+
   g.fillStyle = "#909090";
   g.fillRect(x, y, 32, 32);
-  
+
+  x = clamp(x, 0, 800 - 32);
+  y = clamp(y, 0, 600 - 32);
+
 }
 
 function clamp(val, min, max) {
- 
-  return Math.min(max, Math.max(val, min));
- 
+
+  return Math.max(min, Math.min(val, max));
+
 }
-  
+
 setInterval(loop, 12);
 
 addEventListener("keydown", function(e) {
- 
- if(e.key == "s")
+
+  if (e.key == "ArrowUp")
+    controls.up = true;
+
+  if (e.key == "ArrowRight")
+    controls.right = true;
+
+  if (e.key == "ArrowLeft")
+    controls.left = true;
+
+  if (e.key == "ArrowDown")
     controls.down = true;
-  
+
 });
 
 addEventListener("keyup", function(e) {
- 
- if(e.key == "s")
-   controls.down = false;
- 
+
+  if (e.key == "ArrowUp")
+    controls.up = false;
+
+  if (e.key == "ArrowRight")
+    controls.right = false;
+
+  if (e.key == "ArrowLeft")
+    controls.left = false;
+
+  if (e.key == "ArrowDown")
+    controls.down = false;
+
 });
