6cb63fcb8d6be7519cd33b79d9b87c81ac885f81
PerfectlyCube <93358074+PerfectlyCube@users.noreply.github.com>
2021-10-28 21:43:35 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,14 +1,32 @@
 const canvas = document.getElementById("screen");
 const g = canvas.getContext('2d');
 
+let speed = 10, drag = 100, velX = 0, velY = 0;
+let x = 0, y = 0;
+
 function loop() {
+ 
+  x += velX;
+  y += velY;
+ 
+  velX += speed/drag;
+  velX = clamp(velX, -speed, speed);
+ 
+  velY += speed/drag;
+  velY = clamp(velY, -speed, speed);
  
   g.fillStyle = "#ffffff";
   g.fillRect(0, 0, 800, 600);
   
-  g.fillStyle = "#000000";
+  g.fillStyle = "#0";
   g.fillRect(0, 0, 32, 32);
   
+}
+
+function clamp(val, min, max) {
+ 
+  return Math.min(max, Math.max(val, min));
+ 
 }
   
 setInterval(loop, 12);
