045ab74ee216b2c862e2934c1eb40c20d010c91f
PerfectlyCube <93358074+PerfectlyCube@users.noreply.github.com>
2021-10-29 09:51:52 -0400

    Update main.js

--- main.js
+++ main.js
@@ -4,11 +4,23 @@ const g = canvas.getContext('2d');
 let speed = 10, drag = 100, velX = 0, velY = 0;
 let x = 0, y = 0;
 
+var controls = {
+ 
+  down: false,
+  left: false,
+  right: false,
+  up: false
+ 
+}
+
 function loop() {
  
   g.fillStyle = "#ffffff";
   g.fillRect(0, 0, 800, 600);
   
+  if(controls.down)
+    y += 4;
+ 
   g.fillStyle = "#909090";
   g.fillRect(x, y, 32, 32);
   
@@ -25,12 +37,13 @@ setInterval(loop, 12);
 function addEventListener("keydown", function(e) {
  
  if(e.key == "s")
-    y += 4;
+    controls.down = true;
   
 });
 
 function addEventListener("keyup", function(e) {
  
- 
+ if(e.key == "s")
+   controls.down = false;
  
 });
